60709510f09048bd85e9e696252c25f74606a727
asyncFrank <frank.maldonado.developer@gmail.com>
2023-12-15 13:26:49 -0300

    correcao sppelling Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
         </p>
         <CustomButton
           title="Explorar Carros"
-          containerStyle=" bg-primary-blue text-white rounded-full mt-10"
+          containerStyles=" bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
           btnType="button"
         />
