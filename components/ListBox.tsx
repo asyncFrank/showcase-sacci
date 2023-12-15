b9986d2a732356deadc09ee5e69ae889cf3f3b2b
asyncFrank <frank.maldonado.developer@gmail.com>
2023-12-15 13:32:29 -0300

    remove listbox

--- components/ListBox.tsx
+++ components/ListBox.tsx
@@ -1,33 +1,9 @@
-'use client'
+import React from 'react'
 
-import { useState } from "react";
-import { Listbox } from "@headlessui/react";
-
-const people = [
-  { id: 1, name: "Durward Reynolds" },
-  { id: 2, name: "Kenton Towne" },
-  { id: 3, name: "Therese Wunsch" },
-  { id: 4, name: "Benedict Kessler" },
-  { id: 5, name: "Katelyn Rohan" },
-];
-
-function MyListbox() {
-  const [selectedPeople, setSelectedPeople] = useState([]);
-
+const ListBox = () => {
   return (
-    <Listbox value={selectedPeople} onChange={setSelectedPeople} multiple>
-      <Listbox.Button>
-        {selectedPeople.map((person) => person.name).join(", ")}
-      </Listbox.Button>
-      <Listbox.Options>
-        {people.map((person) => (
-          <Listbox.Option key={person.id} value={person}>
-            {person.name}
-          </Listbox.Option>
-        ))}
-      </Listbox.Options>
-    </Listbox>
-  );
+    <div>ListBox</div>
+  )
 }
 
-export default MyListbox;
+export default ListBox
