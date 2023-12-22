16c55e736f503e86d51e58a445e0fdb577ac5946
asyncFrank <frank.maldonado.developer@gmail.com>
2023-12-22 11:52:07 -0300

    barber

--- utils/index.ts
+++ utils/index.ts
@@ -1,8 +1,8 @@
 import { FilterProps } from "@types";
 import axios from "axios";
 
-const urlBase = "http://localhost:9002/api";
-// const urlBase = "https://sacci-agk.onrender.com/api/";
+// const urlBase = "http://localhost:9002/api";
+const urlBase = "https://sacci-agk.onrender.com/api/";
 //Este atualmente
 export const fetchContacts = async (filters: FilterProps) => {
   const {
